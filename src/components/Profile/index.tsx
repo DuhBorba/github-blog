0b8c7a5e4d1904eb4314bb043b8db39f4b7f4e07
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-09-18 18:01:43 -0300

    Create Profile

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { AvatarContainer, BoxName, InfosGithub, ProfileCard, ProfileContainer, ProfileInfo } from './styles'
+import Avatar from '../../assets/avatar.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBuilding } from '@fortawesome/free-solid-svg-icons/faBuilding'
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons/faUserGroup'
+
+
+export const Profile = () => {
+  return (
+    <ProfileContainer>
+      <ProfileCard>
+        <AvatarContainer>
+          <img src={Avatar} alt="" />
+        </AvatarContainer>
+        
+        <ProfileInfo>
+          <BoxName>
+            <div>
+              <h2>Eduardo Luis Borba</h2>
+            </div>
+            <div>
+              <a href="#" target="_blank">
+                GITHUB
+                <FontAwesomeIcon icon={faUpRightFromSquare} style={{color: "#3294f8",}} />
+              </a>
+            </div>
+          </BoxName>
+
+          <div>
+            <h3>Front-end Developer</h3>
+          </div>
+
+          <InfosGithub>
+            <div>
+            <FontAwesomeIcon icon={faGithub} style={{color: "#3a536b",}} />
+              <p>duhborba</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faBuilding} style={{color: "#3a536b",}} />
+              <p>CRM Services</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserGroup} style={{color: "#3a536b",}} />
+              <p>15 seguidores</p>
+            </div>
+          </InfosGithub>
+        </ProfileInfo>
+
+      </ProfileCard>
+    </ProfileContainer>
+  )
+}
---
+++ src/components/Profile/styles.ts
@@ -0,0 +1,97 @@
+import styled from 'styled-components'
+
+export const ProfileContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+`
+
+export const ProfileCard = styled.div`
+  margin-top: -5.5rem;
+  padding: 2rem 2.5rem;
+  background: ${props => props.theme['base-profile']};
+  border-radius: 10px;
+  box-shadow: 0px 2px 28px 0px rgba(0, 0, 0, 0.20);
+
+  display: grid;
+  grid-template-columns: 14.25rem 1fr;
+  align-items: center;
+`
+
+export const AvatarContainer = styled.div`
+  img{
+    width: 9.25rem;
+    height: 9.25rem;
+    border-radius: 8px;
+  }
+`
+
+export const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+
+  h3{
+    color: ${props => props.theme['base-title']};
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 1.95rem;
+  }
+
+  a{
+    color: ${props => props.theme.blue};
+    font-size: 0.75rem;
+    font-weight: 700;
+    line-height: 1.2rem;
+    text-decoration: none;
+    
+    svg{
+      margin-left: 0.5rem;
+    }
+
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+
+  h3{
+    margin: 0.5rem 0 1.5rem;
+  }
+
+  h3, p{
+    color: ${props => props.theme['base-text']};
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.6rem;
+  }
+`
+
+export const BoxName = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  svg{
+      width: 0.75rem;
+      height: 0.75rem;
+  }
+`
+
+export const InfosGithub = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+
+  div{
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    svg{
+      width: 1.125rem;
+      height: 1.125rem;
+    }
+  }
+`
