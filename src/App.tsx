8b5608bbd97f070f5455f533c9259d4ad741f780
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-09-18 11:22:29 -0300

    Config Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/default";
+import { GlobalStyle } from "./global";
+
 export function App() {
 
   return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
       <p>
         Hello World
       </p>
+    </ThemeProvider>
   )
 }
 
